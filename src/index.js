1ca919c76fbb3728da0eda79197b9abc53f77c92
jscs99 <64388586+jscs99@users.noreply.github.com>
2022-11-02 21:57:29 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>NO elements to show</h1>
+    <p>my parragraph is show here</p>
+  </div>,
+
+  document.getElementById("root")
+);
